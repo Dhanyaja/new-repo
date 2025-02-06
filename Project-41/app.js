5f081ca93ba550ed535fb922ce259fb0bc449c05
Dhanyaja <dhanyaja2003@gmail.com>
2025-02-06 19:15:40 +0530

    Project-41 added

---
+++ Project-41/app.js
@@ -0,0 +1,15 @@
+const container = document.querySelector(".content");
+const baseURL = "https://unsplash.it/1920/1080?random";
+const rows = 7;
+
+for(let i = 0; i < rows * 3; i++){
+    const img = document.createElement("img");
+    img.src = `${baseURL}${randomSize()}`;
+    container.appendChild(img);
+}
+function randomSize(){
+    return `${randomNumber()}x${randomNumber()}`;
+}
+function randomNumber(){
+    return Math.floor(Math.random() * 10) + 300;
+}
